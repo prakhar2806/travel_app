a52f72a6c22343e80eeab85e8ffccd33696c3275
prakhar2806 <prakhar.ngm92@gmail.com>
2024-02-09 00:09:02 +0530

    public folder and new component

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <nav
+      className="border-red-500 border-2 flexBetween max-container
+  padding-container relative z-30 py-5"
+    >
+      Navbar
+    </nav>
+  );
+};
+
+export default Navbar;
